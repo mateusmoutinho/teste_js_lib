ee8b9ddaeabcd364fa29409f4f5c4554ba6396b3
matesmoutinho <matesmoutinho01@gmail.com>
2026-06-15 23:06:27 -0300

    feat: add greet and sub functions to cli module

--- dist/cli.js
+++ dist/cli.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 "use strict";
+function greet(name) {
+    return `Hello, ${name}! This is from the library.`;
+}
+function sub(a, b) {
+    return a - b;
+}
 const args = process.argv.slice(2);
 const aaa = args[0] || 'World';
 console.log(greet(aaa));
